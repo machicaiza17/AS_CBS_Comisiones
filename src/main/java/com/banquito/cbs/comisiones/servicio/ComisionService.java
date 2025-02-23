ad16c8d686707f449577aba3f3267a66b0427502
machicaiza17 <132980301+machicaiza17@users.noreply.github.com>
2025-02-23 15:04:25 -0500

    Implementación de métodos CRUD
    Se implementaron los métodos CRUD correspondientes para el microservicio de Comisiones.

--- src/main/java/com/banquito/cbs/comisiones/servicio/ComisionService.java
+++ src/main/java/com/banquito/cbs/comisiones/servicio/ComisionService.java
@@ -1,13 +1,46 @@
 package com.banquito.cbs.comisiones.servicio;
 
+import com.banquito.cbs.comisiones.modelo.Comision;
 import com.banquito.cbs.comisiones.repositorio.ComisionRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ComisionService {
     private final ComisionRepository comisionRepository;
+    private static final Logger logger = LoggerFactory.getLogger(ComisionService.class);
 
     public ComisionService(ComisionRepository comisionRepository) {
         this.comisionRepository = comisionRepository;
+    }
+
+    public Comision crearComision(Comision comision) {
+        logger.info("Creando comision: {}", comision);
+        return comisionRepository.save(comision);
+    }
+
+    public List<Comision> obtenerComisiones() {
+        logger.info("Obteniendo todas las comisiones");
+        return comisionRepository.findAll();
+    }
+
+    public Optional<Comision> obtenerComisionPorId(Integer id) {
+        logger.info("Obteniendo comision con id: {}", id);
+        return comisionRepository.findById(id);
+    }
+
+    public Comision actualizarComision(Integer id, Comision comision) {
+        logger.info("Actualizando comision con id: {}", id);
+        comision.setId(id);
+        return comisionRepository.save(comision);
+    }
+
+    public void eliminarComision(Integer id) {
+        logger.info("Eliminando comision con id: {}", id);
+        comisionRepository.deleteById(id);
     }
 }
